a0d8f2908427b5b075a76860a498d4a6420d11ae
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-03-25 14:24:28 +0530

    add

---
+++ Module-2/variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+   /*
+        int no = 50;
+        printf("\nValue of no is:%d", no);
+   */
+    char no;
+    printf("Enter the value of no:");
+    scanf("%c", &no);
+    printf("\nValue of no is:%c", no);
+}
