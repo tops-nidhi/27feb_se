9d7b0ad52ec47162ab10e49f981f7f16756061ee
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-04-21 18:54:36 +0530

    add

---
+++ Module-3/Info/GetData.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int id;
+    string nm;
+    cout<<"Enter your id and name:";
+    cin>>id>>nm;
+    cout<<endl<<nm<<" your id is:"<< id;
+    return 0;
+}
